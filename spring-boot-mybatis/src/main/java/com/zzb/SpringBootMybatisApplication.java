717d4aac2c18a67f80d756527793a4186fe4d1b0
IceRain <zhao_zhengbin@126.com>
2017-02-21 17:20:01 +0800

    spring-boot继承mybatis

---
+++ spring-boot-mybatis/src/main/java/com/zzb/SpringBootMybatisApplication.java
@@ -0,0 +1,18 @@
+package com.zzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+}
---
+++ spring-boot-mybatis/src/main/java/com/zzb/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.zzb.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ThinkPad on 2017/2/21.
+ */
+
+@Controller
+public class UserController {
+
+}
